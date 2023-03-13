0a5714bc4b9b74ec8f3017e85516a6f4325f6c3f
Moilanen Tuukka <tuukka.moilanen@edu.espoo.fi>
2023-03-13 14:35:40 +0200

    switched to stable endpoint

--- database/public-api.js
+++ database/public-api.js
@@ -14,7 +14,7 @@ const { resolve } = require('path');
 const url = `mongodb://${user}:${password}@${host}:${port}/?authMechanism=DEFAULT`;
 //const url = `mongodb://127.0.0.1:27017`;
 
-const wilmaAPI = 'https://beta.wilma-api.tuukk.dev/api/';
+const wilmaAPI = 'https://wilma.otawilma.fi/api/';
 //const wilmaAPI = 'http://localhost:3001/api/';
 
 const fetchUserData = (auth) => {
